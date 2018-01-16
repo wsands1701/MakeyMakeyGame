8ed45c42e9ab6a578839cd86f172278bf9659942
wsands1701 <will.sands.v@gmail.com>
2018-01-16 10:20:33 -0500

    Added Useful varibles for both tank location and player score tracking

--- MakeyMakeyGame/src/mainProgram.java
+++ MakeyMakeyGame/src/mainProgram.java
@@ -6,7 +6,12 @@ public class mainProgram extends Applet {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		
+		int tank1x;
+		int tank2x;
+		int tank1y;
+		int tank2y;
+		int player1score;
+		int player2score;
 	}
 	
 	public void paint(Graphics g) 
