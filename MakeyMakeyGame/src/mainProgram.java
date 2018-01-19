de6ed64de130a2656d09ddb4e575d6fdd6ec85b4
wsands1701 <will.sands.v@gmail.com>
2018-01-19 13:42:31 -0500

    Fixed whitepaces

--- MakeyMakeyGame/src/mainProgram.java
+++ MakeyMakeyGame/src/mainProgram.java
@@ -211,22 +211,22 @@ public class mainProgram extends Applet implements KeyListener, ActionListener{
 		
 		
 	}
-  public void update(Graphics g) {
-    Graphics offgc;
-    Image offscreen = null;
-    Dimension d = size();
+    public void update(Graphics g) {
+    	Graphics offgc;
+    	Image offscreen = null;
+    	Dimension d = size();
 
-    // create the offscreen buffer and associated Graphics
-    offscreen = createImage(d.width, d.height);
-    offgc = offscreen.getGraphics();
-    // clear the exposed area
-    offgc.setColor(getBackground());
-    offgc.fillRect(0, 0, d.width, d.height);
-    offgc.setColor(getForeground());
-    // do normal redraw
-    paint(offgc);
-    // transfer offscreen to window
-    g.drawImage(offscreen, 0, 0, this);
+    	// create the offscreen buffer and associated Graphics
+    	offscreen = createImage(d.width, d.height);
+    	offgc = offscreen.getGraphics();
+    	// clear the exposed area
+    	offgc.setColor(getBackground());
+    	offgc.fillRect(0, 0, d.width, d.height);
+    	offgc.setColor(getForeground());
+    	// do normal redraw
+    	paint(offgc);
+    	// transfer offscreen to window
+    	g.drawImage(offscreen, 0, 0, this);
     }
 	
 	public void keyReleased(KeyEvent arg0) {
